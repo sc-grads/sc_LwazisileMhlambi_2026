c9ea36b8eb5a98f93a321fdddf31a46cb147f084
Lwazisile-Sambe <lwazisile.mhlambi@sambeconsulting.com>
2026-03-24 17:17:27 +0200

    Committed Changes for 24/03/2026

---
+++ Linux_Fundamentals/Cheat Sheet.sh
@@ -0,0 +1,90 @@
+# Common Directories
+
+/ #-  “Root”, the top of the file system hierarchy
+
+/bin #- Binaries and other executable programs
+
+/etc #- System configuration files
+
+home #- Home directories
+
+/opt #- Optional or third party software
+
+/tmp #- Temporary space, typically cleared on reboot
+
+/usr #- User related programs
+
+#---------------------------------------------------------
+
+#Basic Linux Commands
+
+#---------------------------------------------------------
+pwd #displays the present working directory
+
+cd [dir] #changes the current directory
+
+ls #list directory contents
+
+ls -l #lists directory in a long listed format
+
+cat #concatenates and displays files
+
+clear #clears the screen
+
+man (manual) [command] #displays the online manual for a command
+
+space #display the next page
+
+q #quit
+
+exit #exits the shell or current session
+
+#---------------------------------------------------------
+
+#Navigating Man Pages
+
+#---------------------------------------------------------
+
+Enter #Move down one line
+
+Space #Move down one page
+
+g #Move to the top of the page
+
+G #Move to the bottom of the page
+
+q #Quit
+
+#---------------------------------------------------------
+
+#Get Help with --help or -h
+
+#---------------------------------------------------------
+
+Add --help #to a command to get help
+
+Try -h #if --help does not work
+
+#---------------------------------------------------------
+
+#Searching Man Pages
+
+#---------------------------------------------------------
+
+man -k #SEARCH_TERM
+
+#Directory Shortcuts
+
+. #this directory
+
+..  #the parent directory
+
+cd - [ENTER] #change to previous directory
+
+#---------------------------------------------------------
+
+#Directory Separator
+
+#---------------------------------------------------------
+
+/ #Directory separator
