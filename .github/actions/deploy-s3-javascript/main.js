0cd795db0f734d56320b07b81a444037d623dd72
Lwazisile-Sambe <lwazisile.mhlambi@sambeconsulting.com>
2026-04-24 12:08:26 +0200

    Section 9 Added Custom JS Action Output Updated

--- .github/actions/deploy-s3-javascript/main.js
+++ .github/actions/deploy-s3-javascript/main.js
@@ -1,6 +1,6 @@
-const core = require('@actions/core')
-const github = require('@actions/github')
-const exec = require('@actions/exec')
+const core = require('@actions/core');
+const github = require('@actions/github');
+const exec = require('@actions/exec');
 
 function run() {
     core.notice('Hello from my custom Javascript Action!');
