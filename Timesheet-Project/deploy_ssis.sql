c75b59207afd4ad350a4c152d81d555c2944353c
Lwazisile-Sambe <lwazisile.mhlambi@sambeconsulting.com>
2026-07-08 17:57:54 +0200

    Wednesday Git Push 08/07/2026 - 11

--- Timesheet-Project/deploy_ssis.sql
+++ Timesheet-Project/deploy_ssis.sql
@@ -12,7 +12,7 @@ SET NOCOUNT ON;
 USE [SSISDB];
 GO
 
-EXECUTE AS LOGIN = 'sa';
+EXECUTE AS LOGIN = 'SAMBE2025008';
 
 DECLARE @folder_name    nvarchar(128)   = N'$(FolderName)';
 DECLARE @project_name   nvarchar(128)   = N'$(ProjectName)';
